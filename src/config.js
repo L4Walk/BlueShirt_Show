a8943ba9f20a6b3e8e9e90ebd74fdccda2aa7f39
LJY_LU <1005058349@qq.com>
2023-10-16 15:21:45 +0800

    更新基础设置

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ const SERVE = false; // 是否使用线上服务器
 
 const HOST = ""; // 线上服务器地址，如果不使用请留空
 
-const BLOG_NAME = "我的博客"; // 个人网站名字
+const BLOG_NAME = "蓝衫科技 生态导航"; // 个人网站名字
 
-const BLOG_URL = "https://blog.esunr.xyz"; // 个人网站链接（请填写完整链接）
+const BLOG_URL = "https://www.bluelsqkj.com/"; // 个人网站链接（请填写完整链接）
 
-const GITHUB = "https://github.com/EsunR"; // 右上角 Github 图标链接，留空不显示
+const GITHUB = ""; // 右上角 Github 图标链接，留空不显示
 
 const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
 
