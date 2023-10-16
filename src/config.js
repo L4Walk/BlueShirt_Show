86ec05648b291641638904b5ccb7b110c5837f4d
LJY_LU <1005058349@qq.com>
2023-10-16 15:27:19 +0800

    更改配置

--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ const SERVE = false; // 是否使用线上服务器
 
 const HOST = ""; // 线上服务器地址，如果不使用请留空
 
-const BLOG_NAME = "蓝衫科技 生态导航"; // 个人网站名字
+const BLOG_NAME = "蓝衫科技 官网"; // 个人网站名字
 
 const BLOG_URL = "https://www.bluelsqkj.com/"; // 个人网站链接（请填写完整链接）
 
 const GITHUB = ""; // 右上角 Github 图标链接，留空不显示
 
-const BACKGROUND_IMG_URL = "https://api.dujin.org/bing/1920.php"; // 背景图片url，默认为必应每日图片（第三方接口）
+const BACKGROUND_IMG_URL = "https://ts1.cn.mm.bing.net/th/id/R-C.ea46e780c79acab0adc06e92964538ee?rik=owa1g2%2bHyBaBHQ&riu=http%3a%2f%2fstatic.58cgg.com%2fuploads%2fallimg%2f201028%2f245-20102R31144601.jpg&ehk=jIXeHZlS8yYgC24OInkc3U%2fzhDwGuTaN%2bChbQjEvmBk%3d&risl=&pid=ImgRaw&r=0"; // 背景图片url，默认为必应每日图片（第三方接口）
 
 const SORT_DATA = [
   { sortId: 1, title: "示例分类1" },
