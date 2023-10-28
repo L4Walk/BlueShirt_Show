366095487ccd8185d348ba6fd5a42c39e144b286
LJY_LU <1005058349@qq.com>
2023-10-28 23:05:13 +0800

    up

--- src/config.js
+++ src/config.js
@@ -21,9 +21,9 @@ const SORT_DATA = [
 const RECORD_NUMBER = ""; // 网站备案号，留空不显示
 
 class LOGO{
-   BLUESHRIT = "http://www.bluelskj.com/wp-content/uploads/2023/05/8fa5b-www.blueshirtyun.com.png",
-   MIDJOURNEY = 'assets/blueshirt_logo/蓝衫midjourney.png',
-   ACDEMIEC = 'assets/blueshirt_logo/学术GPT.png',
+   BLUESHRIT = "http://www.bluelskj.com/wp-content/uploads/2023/05/8fa5b-www.blueshirtyun.com.png";
+   MIDJOURNEY = 'assets/blueshirt_logo/蓝衫midjourney.png';
+   ACDEMIEC = 'assets/blueshirt_logo/学术GPT.png';
 }
 
 
