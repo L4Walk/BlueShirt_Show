a2c33f5294b5d218149d6c773fea88c7d7a1e75e
LJY_LU <1005058349@qq.com>
2023-10-28 23:04:54 +0800

    up

--- src/config.js
+++ src/config.js
@@ -20,11 +20,11 @@ const SORT_DATA = [
 
 const RECORD_NUMBER = ""; // 网站备案号，留空不显示
 
-const LOGO=[
+class LOGO{
    BLUESHRIT = "http://www.bluelskj.com/wp-content/uploads/2023/05/8fa5b-www.blueshirtyun.com.png",
    MIDJOURNEY = 'assets/blueshirt_logo/蓝衫midjourney.png',
    ACDEMIEC = 'assets/blueshirt_logo/学术GPT.png',
-]
+}
 
 
 
