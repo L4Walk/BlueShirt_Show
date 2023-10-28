9f46b867c6c820f4519f068ba899f76f8a449661
LJY_LU <1005058349@qq.com>
2023-10-28 23:30:04 +0800

    up

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const path = require('path');
+
 module.exports = {
   lintOnSave: undefined,
-  productionSourceMap: false, // 取消源代码映射（防止源代码暴露在浏览器控制台的source中的top/webpack://目录下）
+  productionSourceMap: false, // 取消源代码映射
+
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/')  // 这将 '@' 设置为 'src/' 目录的绝对路径
+        // 你还可以添加其他别名，例如:
+        // 'assets': path.resolve(__dirname, 'src/assets/')
+      }
+    }
+  }
 }
